fe4232fcfeb851a111d31890490fe2021ae5308a
LinShiwei <477623614@qq.com>
2016-03-11 00:37:20 +0800

    Revert "Revert "Refactor""
    This reverts commit 78da3e228d817d0d4717b18e27f044dec9c3ba93.

---
+++ MyAdvancedRecorder/MyAdvancedRecorder/Contant.swift
@@ -0,0 +1,22 @@
+//
+//  Contant.swift
+//  MyAdvancedRecorder
+//
+//  Created by Linsw on 16/3/10.
+//  Copyright © 2016年 Linsw. All rights reserved.
+//
+
+import Foundation
+import UIKit
+struct Theme{
+    let navigationBarColor:UIColor
+    let viewBackgroundColor : UIColor
+    let tableViewBackgroundColor:UIColor
+    let tableViewCellBackgroundColor:UIColor
+    let tableViewCellSeletedBackgroundColor:UIColor
+    
+}
+let themeOne = Theme(navigationBarColor: UIColor(hue: 1, saturation: 0.2, brightness: 0.8, alpha: 1),viewBackgroundColor:                 UIColor(hue: 1, saturation: 0.2, brightness: 0.8, alpha: 1),                 tableViewBackgroundColor:            UIColor(hue: 1, saturation: 0.2, brightness: 0.8, alpha: 1),
+    tableViewCellBackgroundColor:        UIColor(hue: 1, saturation: 0.15, brightness: 0.8, alpha: 1),
+    tableViewCellSeletedBackgroundColor: UIColor(hue: 1, saturation: 0.18, brightness: 0.8, alpha: 1)
+)
